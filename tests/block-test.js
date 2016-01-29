a922ae2adde6ddb97e2c54f9b7cc4b5f1e7e3652
David Dias <daviddias.p@gmail.com>
2016-01-29 08:32:15 +0000

    es6ify

--- tests/block-test.js
+++ tests/block-test.js
@@ -5,15 +5,15 @@
 const expect = require('chai').expect
 const Block = require('../src').Block
 
-describe('block', function () {
-  it('block: \t\t create a new block', function (done) {
-    var b = new Block('random-data')
+describe('block', () => {
+  it('block: \t\t create a new block', (done) => {
+    const b = new Block('random-data')
     expect(b.key).to.exist
     expect(b.data).to.exist
     done()
   })
 
-  it('fail to create an empty block', function (done) {
+  it('fail to create an empty block', (done) => {
     var b
     try {
       b = new Block()
@@ -23,17 +23,17 @@ describe('block', function () {
     }
   })
 
-  it('2 different blocks have different hashes', function (done) {
-    var b1 = new Block('random-data')
-    var b2 = new Block('more-random-data')
+  it('2 different blocks have different hashes', (done) => {
+    const b1 = new Block('random-data')
+    const b2 = new Block('more-random-data')
     expect(b1).to.not.deep.equal(b2)
     done()
   })
 
-  it.skip('block stays immutable', function (done) {
+  it.skip('block stays immutable', (done) => {
     // it from the original implementation
     // It doesn't stricly verify the immutability of the Block object
-    var block = new Block("Can't change this!")
+    const block = new Block("Can't change this!")
     var key = block.key
     key = new Buffer('new key')
 
